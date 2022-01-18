4a1c065523f386d2472d57806c302c51be5c551f
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-18 11:36:48 +0200

    feat: add generator

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => (
+  <div className="app__header">
+    <h1 className="app__title">Snippets Syntax generator</h1>
+    <p className="app__subtitle">
+      Made by
+      <a className="app__link" href="#"> Dmitriy Yakovlev</a>
+    </p>
+  </div>
+);
+
+export default Header;
